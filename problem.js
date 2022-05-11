8fc16fd7e7abd8f203c4b66fd2f444fe74068f05
bmills23 <58231400+bmills23@users.noreply.github.com>
2022-05-10 21:26:03 -0600

    Update problem.js

--- problem.js
+++ problem.js
@@ -1,6 +1,6 @@
-We want you to write a function, organizeItems, that organizes items by category. 
+/*We want you to write a function, organizeItems, that organizes items by category. 
 The argument to the function is an array of item objects. Each item object has 3 properties, category (string), itemName (string), and onSale (Boolean).
-Here's an example:
+Here's an example:*/
 
 var itemData = [
   { category: 'fruit',  itemName: 'apple', onSale: false },
@@ -11,15 +11,16 @@ var itemData = [
   { category: 'canned', itemName: 'soup',  onSale: false },
 ];
 
-The return value should be an object with category properties. Each property value is an array of items that belong to that category.
-Here's an example return object based on our example input:
+/*The return value should be an object with category properties. Each property value is an array of items that belong to that category.
+Here's an example return object based on our example input:*/
 
 {
   fruit:  ['apple', 'melon($)'],
   canned: ['beans', 'corn($)', 'soup'],
   frozen: ['pizza']
 };
-Note that items having onSale set to true should have '($)' appended to their item name.
+
+/*Note that items having onSale set to true should have '($)' appended to their item name.
 
 Please complete the function organizeItems and validate it is working by running the tests.
-Your function should work for any similarly formatted input data, not just the example data we've provided.
+Your function should work for any similarly formatted input data, not just the example data we've provided.*/
